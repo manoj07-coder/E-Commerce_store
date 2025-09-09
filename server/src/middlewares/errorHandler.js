ea3ccc36b452898781c29125b26279bb60ca8f22
Manoj S G <manojgowdacs35@gmail.com>
2025-09-09 18:32:27 +0530

    setting up centralized error class

---
+++ server/src/middlewares/errorHandler.js
@@ -0,0 +1,12 @@
+import { logger } from "../utils/logger.js";
+import { AppError } from "../utils/appError.js";
+import { fail } from "../utils/apiResponse.js";
+
+export const errorHandler = (err, _req, res, _next) => {
+  if (!(err instanceof AppError)) {
+    logger.error({ err }, "Unhandled Error");
+  }
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json(fail(message));
+};
