17da313b69ca87d90e456c57b756664a3909f275
Manoj S G <manojgowdacs35@gmail.com>
2025-09-10 13:11:51 +0530

    created auth middleware for role based access

---
+++ server/src/middlewares/auth.js
@@ -0,0 +1,35 @@
+import { AppError } from "../utils/AppError.js";
+import jwt from "jsonwebtoken";
+import { ENV } from "../config.js";
+import User from "../models/User.js";
+
+export async function authGuard(req, res, next) {
+  try {
+    const auth = req.headers.authorization;
+    if (!auth || !auth.startsWith("Bearer ")) {
+      throw new AppError("Not Authenticated", 401);
+    }
+    const token = auth.split(" ")[1];
+    const payload = jwt.verify(token, ENV.JWT_SECRET);
+    const user = await User.findById(payload.sub).select("-password");
+    if (!user) {
+      throw new AppError("User not found", 400);
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function roleGuard(roleNames = []) {
+  return (req, res, next) => {
+    if (!req.user) {
+      throw new AppError("Not Authenticated", 401);
+    }
+    if (!roleNames.includes(req.user.role)) {
+      return next(new AppError("Forbidden", 403));
+    }
+    next();
+  };
+}
