05d5074f712bc229832b454a7b5dcf5807104c77
Manoj S G <manojgowdacs35@gmail.com>
2025-09-15 21:56:08 +0530

    created review model

---
+++ server/src/models/Review.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const ReviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+    rating: {
+      type: Number,
+      comment: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Review", ReviewSchema);
